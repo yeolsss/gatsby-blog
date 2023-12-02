fad43b3ccdf8f9d4200b027dfbbaf60dc1da9914
yeolsss <yeol8810@gmail.com>
2023-12-02 21:28:17 +0900

    [fix] context파일 내에 localStorage가 클라이언트 렌더링일 때만 사용 가능하게 변경

--- src/context/ToggleContext.tsx
+++ src/context/ToggleContext.tsx
@@ -1,68 +1,115 @@
 // init settings
-import * as React from "react";
-import { createContext, useContext, useState } from "react";
+import * as React from "react"
+import {
+  createContext,
+  useContext,
+  useState,
+} from "react"
 
 export interface IThemeContext {
-  themeState: string;
-  profileState: string;
-  toggleTheme: () => void;
-  toggleProfile : () => void;
+  themeState: string
+  profileState: string
+  toggleTheme: () => void
+  toggleProfile: () => void
 }
 
-interface ILocalStorage{
-  themeState: string;
-  profileState: string;
+interface ILocalStorage {
+  themeState: string
+  profileState: string
 }
 
-interface IToggleProviderProps extends React.PropsWithChildren<{}> {}
+interface IToggleProviderProps
+  extends React.PropsWithChildren<{}> {}
 
-const BLOG_TOGGLE = "blog_toggle";
+const BLOG_TOGGLE = "blog_toggle"
 
 const initialState: IThemeContext = {
   themeState: "lightTheme",
   profileState: "open",
   toggleTheme: () => {},
   toggleProfile: () => {},
-};
+}
 // create provider
-export const ToggleContext = createContext(initialState);
+export const ToggleContext =
+  createContext(initialState)
 
 // create context
 
-export function ToggleContextProvider({ children }: IToggleProviderProps) {
+export function ToggleContextProvider({
+  children,
+}: IToggleProviderProps) {
+  const isBrowser =
+    typeof window !== "undefined"
 
-  const localToggleState =  JSON.parse(localStorage.getItem(BLOG_TOGGLE)!)||{themeState: "lightTheme",
-  profileState: "open",
-} as ILocalStorage
+  const localToggleState = isBrowser
+    ? JSON.parse(
+        localStorage.getItem(
+          BLOG_TOGGLE,
+        )!,
+      ) || {
+        themeState: "lightTheme",
+        profileState: "open",
+      }
+    : {
+        themeState: "lightTheme",
+        profileState: "open",
+      }
 
-  const [themeState, setThemeState] = useState(
-    localToggleState.themeState,
-  );
-  const [profileState, setProfileState] = useState(
+  const [themeState, setThemeState] =
+    useState(
+      localToggleState.themeState,
+    )
+  const [
+    profileState,
+    setProfileState,
+  ] = useState(
     localToggleState.profileState,
-  );
-  
+  )
+
   const toggleTheme = () => {
-    const settingTheme = themeState === "lightTheme" ? "darkTheme" : "lightTheme";
-    localStorage.setItem(BLOG_TOGGLE, JSON.stringify({...localToggleState, themeState: settingTheme}));
-    setThemeState(settingTheme);
-  };
+    const settingTheme =
+      themeState === "lightTheme"
+        ? "darkTheme"
+        : "lightTheme"
+    localStorage.setItem(
+      BLOG_TOGGLE,
+      JSON.stringify({
+        ...localToggleState,
+        themeState: settingTheme,
+      }),
+    )
+    setThemeState(settingTheme)
+  }
 
   const toggleProfile = () => {
-    const settingProfile = profileState === "open" ? "close" : "open";
-    localStorage.setItem(BLOG_TOGGLE, JSON.stringify({...localToggleState, profileState: settingProfile}));
-    setProfileState(settingProfile);
+    const settingProfile =
+      profileState === "open"
+        ? "close"
+        : "open"
+    localStorage.setItem(
+      BLOG_TOGGLE,
+      JSON.stringify({
+        ...localToggleState,
+        profileState: settingProfile,
+      }),
+    )
+    setProfileState(settingProfile)
   }
 
   const value = {
     themeState,
     profileState,
     toggleTheme,
-    toggleProfile
-  };
+    toggleProfile,
+  }
   return (
-    <ToggleContext.Provider value={value}>{children}</ToggleContext.Provider>
-  );
+    <ToggleContext.Provider
+      value={value}
+    >
+      {children}
+    </ToggleContext.Provider>
+  )
 }
 
-export const useToggleContext = () => useContext(ToggleContext);
+export const useToggleContext = () =>
+  useContext(ToggleContext)
